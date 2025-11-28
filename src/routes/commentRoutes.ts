e88eca839955e858738b3b3137e5af09d461496f
benbenSapir <benbenfsapir@gmail.com>
2025-11-28 22:06:08 +0200

    fix: standards

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -4,9 +4,9 @@ import commentController from "../controllers/commentController";
 const router = Router();
 
 router.get("/", commentController.getAll.bind(commentController));
-router.get("/:id", commentController.getById.bind(commentController));
 router.get("/post/:postId", commentController.getByPost.bind(commentController));
 router.get("/sender/:senderId", commentController.getBySender.bind(commentController));
+router.get("/:id", commentController.getById.bind(commentController));
 router.post("/", commentController.create.bind(commentController));
 router.put("/:id", commentController.update.bind(commentController));
 router.delete("/:id", commentController.delete.bind(commentController));
